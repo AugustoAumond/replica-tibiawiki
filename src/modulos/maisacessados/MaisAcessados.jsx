3a252ad5164e9894e06f6ffe4633587e060f073a
AugustoAumond <80125507+AugustoAumond@users.noreply.github.com>
2021-11-24 11:00:35 -0300

    Add files via upload

---
+++ src/modulos/maisacessados/MaisAcessados.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import styled from "styled-components";
+const user = `${process.env.PUBLIC_URL}/imagens/Usuario.png`;
+console.log('TESTE ENV =>', process.env);
+
+
+
+function MaisAcessados(){
+    return (
+        <Corpo>
+                ashuahsuas
+
+        </Corpo>
+    )
+}
+
+export default MaisAcessados
+
+const Corpo = styled.div`
+display:flex;
+width: 82%;
+border: solid;
+`
